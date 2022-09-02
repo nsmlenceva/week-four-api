cd9bed373e927064edc49ddc39a4306df08f3208
nsmlenceva <nsmlenceva@rambler.ru>
2022-09-02 07:09:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    content: "",
+    author: ""
+  }
+async componentDidMount() {
+    const url = "https://api.quotable.io/random";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({content: data.content, author: data.author});
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.content}</p>
+        <p>{this.state.author}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
